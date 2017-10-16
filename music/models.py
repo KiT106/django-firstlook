a8540e7e2168a503d253e2b4dd3b09a0204c121d
Dũng Đặng Minh <dung.dm@teko.vn>
2017-10-16 18:48:14 +0700

    Admin for `music`

--- music/models.py
+++ music/models.py
@@ -8,6 +8,9 @@ class Album(models.Model):
     album_title = models.CharField(max_length=250)
     album_logo = models.CharField(max_length=10000)
 
+    def __str__(self):
+        return self.album_title + ' - ' + self.artist
+
 
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
